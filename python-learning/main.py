1ef7eac1120f768e79e6e0dc658c0838a354e997
dhiru-byte <dhrnd.ydv@gmail.com>
2022-06-16 23:47:32 +0530

    Adding python-learning

---
+++ python-learning/main.py
@@ -0,0 +1,26 @@
+# a = 50
+# b = 5
+
+# a = float(input("Enter a :"))
+# b = float(input("Enter b :"))
+
+
+# print("Division : ", a / b)
+
+# print("Multiplication : ", a * b)
+
+# print("Addition : ", a + b)
+
+# print("Subtraction : ", a - b)
+
+# if a > b:
+#   print("a is greater than b")
+# else:
+#   print("b is greater than a")
+
+
+a_list = []
+# for num in range(10):
+    list_num = float(input("Enter a number:"))
+    a_list.append(list_num)
+print(sum(a_list))
