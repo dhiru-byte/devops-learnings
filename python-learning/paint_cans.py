bdc256efea328747fce18b94991ac3725de54ab8
Dhirendra <dhirendra@192.168.1.26>
2023-09-10 12:23:10 +0530

    paint_cans.py

--- python-learning/paint_cans.py
+++ python-learning/paint_cans.py
@@ -1,8 +1,8 @@
-import math
+import math       #to round up can number
 def no_of_can(h, w, cov):
     area = h*w
-    can = str(math.ceil(area / cov))
+    can = str(math.ceil(area / cov))  #round_up of can calculation.
     
-    print("No of cans :" + can)
+    print("No of cans to cover paint:" + can)
 
 no_of_can(h=100, w=6, cov=20)
