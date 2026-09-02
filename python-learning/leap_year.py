4524ac735884572e71679286a45dc042045c87c7
Dhirendra Yadav <dhirendra.yadav@stem.com>
2026-09-02 13:34:48 +0530

    Remove duplicate python-learning directory.

--- python-learning/leap_year.py
+++
@@ -1,6 +0,0 @@
-year = int(input("Enter a Year : "))
-
-if((year % 400 == 0) | (year % 100 != 0) & (year % 4 == 0)):
-  print("Is a Leap Year") 
-else:
-  print("Not a Leap Year")  
--- python-learning/type_conversion.py
+++
@@ -1,4 +0,0 @@
-name = input("what is your name?")
-length = len(name)
-str_len = str(length)
-print("Your Name has "+ str_len +" Char")
